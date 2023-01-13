5ba66885d55930f23e1149aaf8de48b741a8ed06
MihHiu chu ai <himayato.2019.vn@gmail.com>
2023-01-13 09:09:57 +0700

    build base App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        title: "Counter App chu gi nua!",
+        home: Center(
+          child: Text("App counter number!"),
+        ));
+  }
 }
