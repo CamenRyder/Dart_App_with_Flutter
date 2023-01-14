d4c3fa417078129d8d84af981a13386b45e6e8f0
MihHiu chu ai <himayato.2019.vn@gmail.com>
2023-01-14 10:24:04 +0700

    base infinte comment

---
+++ lib/MyApp.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Infinte App use builder !",
+      home: Scaffold(
+          body: SafeArea(
+        minimum: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemBuilder: ((context, index) {
+            return ListTile(
+              leading: Text(
+                " ${index + 1} ",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              title: Text(
+                "Goodmorning, guys!",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              isThreeLine: true,
+              subtitle: Text(
+                "Today is 14/1 you have to learning about tutorial message heyy:)",
+                style: TextStyle(color: Colors.black, fontSize: 18),
+              ),
+            );
+          }),
+          itemCount: 50,
+        ),
+      )),
+    );
+  }
+}
