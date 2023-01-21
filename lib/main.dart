4a15daed0af772a1d18a70c7b00f9012ec635bc4
MihHiu chu ai <himayato.2019.vn@gmail.com>
2023-01-21 07:39:46 +0700

    Update github

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/comment_bloc.dart';
+import 'events/comment_events.dart';
+import 'infinite_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: BlocProvider(
+        create: (context) => CommentBloc()..add(CommentFetchedEvent()),
+        child: InfiniteList(),
+      )
+    );
+  }
+}
